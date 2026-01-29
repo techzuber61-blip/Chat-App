5046a59f8365cc591bcf72efe2a56c292ebcfe8b
Zuber <techzuber61@gmail.com>
2026-01-30 01:08:04 +0530

    2

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,9 +7,11 @@ import messageRoutes from './routes/message.route.js';
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 
