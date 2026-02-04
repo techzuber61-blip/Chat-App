e31c79e41bd8b323aac7a2ac55221f56e923ef99
Zuber <techzuber61@gmail.com>
2026-02-05 00:10:31 +0530

    axios url changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000" : "/",
     withCredentials: true
 })
