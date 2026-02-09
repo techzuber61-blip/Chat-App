8b1845b2e126d22fd21234ff96ad3f1fc64b4d46
Zuber <techzuber61@gmail.com>
2026-02-09 23:57:48 +0530

    chat page setup

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,35 @@
-import { useAuthStore } from "../store/useAuthStore";
+import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
+import { useChatStore } from "../store/useChatStore";
+import ActiveTabSwitch from "../components/ActiveTabSwitch";
+import ChatList from "../components/ChatList";
+import ContactList from "../components/ContactList";
+import ProfileHeader from "../components/ProfileHeader";
+import ChatContainer from "../components/ChatContainer";
+import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
 
 function ChatPage() {
-  const {logout} = useAuthStore();
+  const {activeTab, selectedUser} = useChatStore();
   return (
-    <div>ChatPage
+    <div className="relative w-full max-w-6xl h-[800px]">
+      <BorderAnimatedContainer>
+        <div className="w-full flex flex-col md:flex-row">
+          {/* FORM CLOUMN - LEFT SIDE */}
+          <ProfileHeader />
+          <ActiveTabSwitch />
 
-      <button onClick={logout} className="z-50 absolute top-4 right-4">Logout</button>
+          <div className="flex-1 overflow-y-auto p-4 space-y-2">
+            {activeTab === "chats" ? <ChatList /> : <ContactList />}
+          </div>
+
+          {/* FORM CLOUMN - RIGHT SIDE */}
+          <div className="flex-1 flex flex-col bg-slate-900">
+            {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
+          </div>
+          
+        </div>
+      </BorderAnimatedContainer>
+
+      
     </div>
   )
 }
